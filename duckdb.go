b05cbf08e72a23b53f6c17fffca29c1d1eb7f63d
czyt <czyt@w.cn>
2024-12-13 17:46:12 +0800

    refactor

--- duckdb.go
+++ duckdb.go
@@ -1,1 +1,71 @@
 package duckgorm
+
+import (
+	_ "github.com/marcboeker/go-duckdb"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+	"gorm.io/gorm/schema"
+)
+
+const (
+	driverName = "duckdb"
+)
+
+var _ gorm.Dialector = (*Dialector)(nil)
+
+type Dialector struct {
+	*Config
+}
+
+type Config struct {
+	DriverName string
+	DSN        string
+	Conn       gorm.ConnPool
+}
+
+func Open(dsn string) gorm.Dialector {
+	return &Dialector{&Config{DSN: dsn}}
+}
+
+func New(config Config) gorm.Dialector {
+	return &Dialector{Config: &config}
+}
+
+func (d *Dialector) Name() string {
+	return driverName
+}
+
+func (d *Dialector) Initialize(db *gorm.DB) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *Dialector) Migrator(db *gorm.DB) gorm.Migrator {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *Dialector) DataTypeOf(field *schema.Field) string {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *Dialector) DefaultValueOf(field *schema.Field) clause.Expression {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *Dialector) BindVarTo(writer clause.Writer, stmt *gorm.Statement, v interface{}) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *Dialector) QuoteTo(writer clause.Writer, s string) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *Dialector) Explain(sql string, vars ...interface{}) string {
+	//TODO implement me
+	panic("implement me")
+}
